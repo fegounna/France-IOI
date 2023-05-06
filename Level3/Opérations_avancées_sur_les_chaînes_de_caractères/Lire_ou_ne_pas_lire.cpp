201307f2c0fd60d106a6c1ea558b37f8af5e7404
Yessin Moakher <130999532+fegounna@users.noreply.github.com>
2023-05-06 14:04:11 +0200

    Create Lire_ou_ne_pas_lire.cpp

---
+++ Level3/Opérations_avancées_sur_les_chaînes_de_caractères/Lire_ou_ne_pas_lire.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+string s;
+vector<string> v;
+int n;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n;
+    getline(cin,s);
+    for(int i=0;i<n;++i)
+    {
+      getline(cin,s);
+      int j;
+      for(j=0;j<v.size();++j)
+         if(v[j]>s)
+            break;
+      if(j==v.size())
+      {
+         cout<<s<<endl;
+         v.push_back(s);
+      }
+    }
+    return 0;
+}
